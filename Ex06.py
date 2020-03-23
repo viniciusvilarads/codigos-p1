835e1bfc758d2cee2216def4c664a060103737d8
Vinícius Vilar <viniciusmorais@ads.fiponline.edu.br>
2020-03-23 18:23:45 -0300

    Removendo arquivos

--- Ex06.py
+++
@@ -1,8 +0,0 @@
-"""
-6. Escreva um programa que leia um valor inteiro e calcule o seu quadrado.
-"""
-
-num = int(input("Informe o número inteiro: "))
-numElevado = num ** 2
-
-print("{} ao quadrado é {}".format(num, numElevado))
--- Ex09.py
+++
@@ -1,9 +0,0 @@
-"""
-9. Escreva um programa que converte valores de polegadas em centímetros
-utilizando a seguinte fórmula para conversão: 1 polegada = 2,54 cm;
-"""
-
-pol = float(input("Informe o valor em polegadas: "))
-cent = pol * 2.54
-
-print("{}pol é igual a {:.2f}cm".format(pol, cent))
